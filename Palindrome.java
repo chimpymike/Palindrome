74a8dfea3b70259549835c2154c5f22b005cafba
Mike Callahan <chimpymike@gmail.com>
2013-02-18 08:27:59 -0500

    Add main method for testing isPalindrome method.

---
+++ Palindrome.java
@@ -0,0 +1,11 @@
+class Palindrome {
+    public static void main(String[] args) {
+	String testPalindrome = "tenet";
+	if (isPalindrome(testPalindrome)) {
+	    System.out.println("This IS a palindrome.");
+	} else {
+	    System.out.println("This is NOT a palindrome.");
+	}
+    }
+
+    
