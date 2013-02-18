e28ba4e0410979a699e0a4fc246992b938c85b8f
Mike Callahan <chimpymike@gmail.com>
2013-02-18 08:37:39 -0500

    Ignore first character if it is NOT a letter.

--- Palindrome.java
+++ Palindrome.java
@@ -20,4 +20,15 @@ class Palindrome {
 	    lastChar = Character.toLowerCase(s.charAt(s.length()-1));
 	    System.out.print(firstChar);
 	    System.out.println(lastchar);
-	    
+
+	    if (!Character.isLetter(firstChar)) {
+		if (!Character.isLetter(lastChar)) {
+		    // first and last are both not letters so ignore them
+		    System.out.println("Both are not letters.");
+		    return isPalindrome(s.substring(1, s.length()-1));
+		} else {
+		    // first is not a letter so ignore
+		    System.out.println("firstChar is NOT a letter");
+		    return isPalindrome(s.substring(1, s.length()));
+		}
+	    }
